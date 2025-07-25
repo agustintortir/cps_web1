faa1f7c1a9ae15705994f3f08928e132ca69995c
Local-Admin <Local-Admin@U3639-CE-008>
2025-07-25 12:56:11 -0300

    app.net

---
+++ Practica.NET/PrimeraAppMVC/Visitas/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Visitas.Models;
+
+namespace Visitas.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        public ActionResult CargaDeDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            string comentarios = Request.Form["comentarios"].ToString();
+            LibroDeVisitas libro = new LibroDeVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+    }
+}
---
+++ Practica.NET/PrimeraAppMVC/Visitas/Models/LibroDeVisitas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Visitas.Models
+{
+    public class LibroDeVisitas
+    {
+        public void Grabar(string nombre, string comentarios)
+        {
+            StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt", true);
+            archivo.WriteLine("Nombre:" + nombre + "<br>Comentarios:" + comentarios + "<hr>");
+            archivo.Close();
+        }
+    }
+}
