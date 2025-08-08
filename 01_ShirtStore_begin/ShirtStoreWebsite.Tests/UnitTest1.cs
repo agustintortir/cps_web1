42f631c74775d382df882f55bcbd1fa23c309a76
Local-Admin <Local-Admin@U3639-CE-008>
2025-08-08 12:53:12 -0300

    modulo 9

---
+++ 01_ShirtStore_begin/ShirtStoreWebsite.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using ShirtStoreWebsite.Models;
+
+namespace ShirtStoreWebsite.Tests
+{
+    public class Temp
+    {
+        public void Test()
+        {
+            var shirt = new Shirt(); // ¿Sigue marcando error aquí?
+        }
+    }
+}
